5c88f6c0d8879096b1130aa73889363cf70a9e29
Bala KS <ksbala8@gmail.com>
2022-01-18 08:13:16 +0530

    learned - class initialisation - adding Scaffold - material design widgets - adding App Bar - adding column / row list - adding raisedbutton

--- project_1/lib/main.dart
+++ project_1/lib/main.dart
@@ -5,10 +5,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var question = [
+    'What\'s your favorite color',
+    'what\'s your favorite animal'
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question 1'),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+          ],
+        ),
+      ),
     );
   }
 }
